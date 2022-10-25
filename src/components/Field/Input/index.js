1a558f42b64ef270b22e2b3521ca19c9296fcdb6
Anton Sivagin <ansn.onward@gmail.com>
2022-10-25 19:05:53 +0300

    refactor: correct input component

--- src/components/Field/Input/index.js
+++ src/components/Field/Input/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-const Input = ({ title, field, form }) => {
+const Input = ({ field, form, title, type = 'text' }) => {
   const { name } = field;
   const { setFieldValue } = form;
-  console.log(field, form)
+
   return (
   <div className={styles.container}>
     <label className={styles.label}>{title}</label>
-    <input className={styles.input} onChange={({ target: { value}}) => setFieldValue(name, value ? value : null)} />
+    <input className={styles.input} type={type} onChange={({ target: { value}}) => setFieldValue(name, value ? value : null)} />
   </div>
   )
 };
