6505c30af42c008d25cda224c4c0d2dd86a63086
Anton Sivagin <ansn.onward@gmail.com>
2022-10-25 22:24:17 +0300

    refactor: correct input component

--- src/components/Field/Input/index.js
+++ src/components/Field/Input/index.js
@@ -8,7 +8,7 @@ const Input = ({ field, form, title, type = 'text' }) => {
   return (
   <div className={styles.container}>
     <label className={styles.label}>{title}</label>
-    <input className={styles.input} type={type} onChange={({ target: { value}}) => setFieldValue(name, value ? value : null)} />
+    <input className={styles.input} type={type} onChange={({ target: { value }}) => setFieldValue(name, value)} />
   </div>
   )
 };
