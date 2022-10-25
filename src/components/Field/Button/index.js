bc9d121f711aa44d6962e9cf4ea894597b423450
Anton Sivagin <ansn.onward@gmail.com>
2022-10-25 22:24:03 +0300

    feat: update button

--- src/components/Field/Button/index.js
+++ src/components/Field/Button/index.js
@@ -1,28 +1,50 @@
 import React, { useState }  from 'react';
 import styles from './styles.module.css';
 
-const Button = ({ isValid }) => {
+const getError = errors => {
+  const values = Object.values(errors);
+  if (values.length === 0) return null;
+  return values[0]
+}
+
+const Button = ({ errors }) => {
   const [isHover, setIsHovering] = useState(false)
 
   const handleMouseEnter = () => setIsHovering(true);
   const handleMouseLeave = () => setIsHovering(false);
 
-  const setHoverStyles = () => {
-    const showButton = { opacity: 1, cursor: 'pointer' }
-    const hideButton = { opacity: 0, cursor: 'auto' }
+  const setHoverInputStyles = () => {
+    const showButton = { transform: 'perspective(600px) rotateY(0deg)' }
+    const hideButton = { transform: 'perspective(600px) rotateY(-180deg)' }
 
-    if (isHover) return isValid ? showButton : hideButton;
+    if (isHover) return error === null ? showButton : hideButton;
     return showButton;
   }
 
+  const setHoverErrorStyles = () => {
+    const showError = { transform: 'perspective(600px) rotateY(0deg)' }
+    const hideError = { transform: 'perspective(600px) rotateY(180deg)' }
+
+    if (isHover) return error !== null ? showError : hideError;
+    return hideError;
+  }
+
+  const error = getError(errors);
+
   return (
-    <input
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      className={styles.input}
-      type="submit"
-      value="Войти"
-      style={setHoverStyles()} />
+  <div className={styles.container} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+    {/*<div className={styles.card}>*/}
+      <input
+        className={styles.input}
+        type="submit"
+        value="Войти"
+        style={setHoverInputStyles()}
+      />
+      <div className={styles.error} style={setHoverErrorStyles()}>
+        <span>{error}</span>
+      </div>
+    {/*</div>*/}
+  </div>
   )
 };
 
