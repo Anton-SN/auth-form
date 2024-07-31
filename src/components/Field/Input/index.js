64a6bd8212561a2edf50716d3e07529212f16561
Anton Sivagin <ansn.onward@gmail.com>
2024-07-31 13:35:27 +0200

    fix: correct after review

--- src/components/Field/Input/index.js
+++ src/components/Field/Input/index.js
@@ -7,8 +7,8 @@ const Input = ({ field, form, title, type = 'text' }) => {
 
   return (
   <div className={styles.container}>
-    <label className={styles.label}>{title}</label>
-    <input className={styles.input} type={type} value={value} onChange={({ target: { value }}) => setFieldValue(name, value)} />
+    <label className={styles.label} htmlFor={name}>{title}</label>
+    <input className={styles.input} id={name} type={type} value={value} onChange={({ target: { value }}) => setFieldValue(name, value)} />
   </div>
   )
 };
